3042b787f560f4e17828a0be0bda4274187d1a67
Michael Baker <pxperfectmike@gmail.com>
2022-08-29 19:03:02 -0700

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">Mo'LoVE</div>
+            <div className="link-container">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">collections</a></li>
+                    <li><a href="#">Trims and cuts</a></li>
+                    <li><a href="#">fitness and training</a></li>
+                    <li><a href="#">about us</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
